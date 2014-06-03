2383aa2addf00226ddc8dc4f961aa0875738e370
Marroquin Estrada Miguel <marroquin@marroquin-eM355.(none)>
2014-06-03 17:03:31 -0500

    Agregando Urls al dashboard

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -8,6 +8,7 @@ class UrlsController < ApplicationController
   	 if !params[:url].blank?
       client = Bitly.client
       @url = client.shorten(params[:url])
+      BitLyUrl.create!(url: @url.short_url, user_id: current_user.id)#o url como se te hinche
     end
   end
 end
--- app/models/bit_ly_url.rb
+++ app/models/bit_ly_url.rb
@@ -1,3 +1,10 @@
 class BitLyUrl < ActiveRecord::Base
 	belongs_to :user
+	def counter
+    bitly_client.clicks(url).global_clicks
+  end
+
+	def bitly_client
+	  @bitly_client ||= Bitly.client
+	end
 end
