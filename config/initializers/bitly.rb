787694022b09b4a8654be2b01309cb54d98cf785
Miguel Marroquin Estrada <marroquin.em93@gmail.com>
2014-06-03 17:30:03 -0500

    Update bitly.rb

--- config/initializers/bitly.rb
+++ config/initializers/bitly.rb
@@ -2,7 +2,7 @@ Bitly.configure do |config|
 	#Configurando version de Bit.ly
   config.api_version = 3
   #Usuario de Bit.ly
-  config.login = "marroking"
+   config.login = env["USER_BITLY"]
   #Key de Bit.ly
   config.api_key = env["BITLY_API"]
 end
