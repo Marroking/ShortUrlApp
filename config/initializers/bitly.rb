ed0ec8d56eb13b99f691f9fb187f239821f4504e
Marroquin Estrada Miguel <marroquin@marroquin-eM355.(none)>
2014-06-03 17:52:22 -0500

    Removing API Key

--- config/initializers/bitly.rb
+++ config/initializers/bitly.rb
@@ -2,7 +2,7 @@ Bitly.configure do |config|
 	#Configurando version de Bit.ly
   config.api_version = 3
   #Usuario de Bit.ly
-   config.login = env["USER_BITLY"]
+   config.login = ENV["USER_BITLY"]
   #Key de Bit.ly
-  config.api_key = env["BITLY_API"]
+  config.api_key = ENV["BITLY_API"]
 end
